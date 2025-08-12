a6f9b27c89f251775609cdd72f88d081bf5dff1a
syedwajid-mahboob <wajidmahboobf23@nutech.edu.pk>
2025-08-12 23:13:30 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('clickBtn').addEventListener('click', () => {
+  const message = document.getElementById('message');
+  message.textContent = 'Button clicked! Thanks for interacting, Wajid.';
+});
